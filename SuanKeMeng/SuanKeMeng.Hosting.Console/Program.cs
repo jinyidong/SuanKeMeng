c1c1ba1e6e04de8cc5917a1b90cf8cd7232dec2e
jinyidong <jinyidong@suiyi.com.cn>
2017-08-04 20:02:13 +0800

    Sln

---
+++ SuanKeMeng/SuanKeMeng.Hosting.Console/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SuanKeMeng.Hosting.Console
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //开启服务
+            new Core.ApplicationBuilder()
+                .UseDataServer(() => new Core.Server.ServerOptions() { BossGroup = 2, WorkerGroup = 8, Port = 8092 })
+                .Build()
+                .Run();
+            System.Console.Read();
+
+        }
+    }
+}
