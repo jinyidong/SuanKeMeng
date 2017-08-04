e4d167fc4551dba3c2116009a644130b441a6510
jinyidong <jinyidong@suiyi.com.cn>
2017-08-04 20:01:44 +0800

    Service层

---
+++ SuanKeMeng/SuanKeMeng.Services/AccountHandler.cs
@@ -0,0 +1,72 @@
+﻿using Core;
+using Core.Events;
+using SuanKeMeng.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace SuanKeMeng.Services
+{
+    public class AccountHandler : ModelEventHandler
+    {
+        protected override void InitializeComponents()
+        {
+            AddEventListener<Create<Account>>(OnCreate);
+            AddEventListener<Update<Account>>(OnUpdate);
+            AddEventListener<Delete<Account>>(OnDelete);
+            AddEventListener<Select<Account>>(OnSelect);
+        }
+        protected virtual async Task<Create<Account>> OnCreate(Create<Account> eventData,
+            CallNext<Create<Account>> callnext)
+        {
+            var model = eventData.Model;
+
+            if (model == null)
+                throw new ArgumentNullException("model");
+
+            eventData.ReturnValue = await DataService.CreateAsync(model);
+
+            return await callnext(eventData);
+        }
+
+        protected virtual async Task<Update<Account>> OnUpdate(Update<Account> eventData,
+           CallNext<Update<Account>> callnext)
+        {
+            var model = eventData.Model;
+
+            if (model == null)
+                throw new ArgumentNullException("model");
+
+            eventData.ReturnValue = await DataService.UpdateAsync(model);
+
+            return await callnext(eventData);
+        }
+
+        protected virtual async Task<Delete<Account>> OnDelete(Delete<Account> eventData,
+            CallNext<Delete<Account>> callnext)
+        {
+            var model = eventData.Model;
+
+            if (model == null)
+                throw new ArgumentNullException("model");
+
+            eventData.ReturnValue = await DataService.DeleteAsync(model);
+
+            return await callnext(eventData);
+        }
+
+        protected virtual async Task<Select<Account>> OnSelect(Select<Account> eventData,
+            CallNext<Select<Account>> callnext)
+        {
+            var query = eventData.Query;
+
+            if (query == null)
+                throw new ArgumentNullException("query");
+
+            eventData.ReturnValue = await DataService.SelectAsync(query);
+
+            return await callnext(eventData);
+        }
+
+    }
+
+}
---
+++ SuanKeMeng/SuanKeMeng.Services/SectorHandler.cs
@@ -0,0 +1,72 @@
+﻿using Core;
+using Core.Events;
+using SuanKeMeng.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace SuanKeMeng.Services
+{
+    public class SectorHandler : ModelEventHandler
+    {
+        protected override void InitializeComponents()
+        {
+            AddEventListener<Create<Sector>>(OnCreate);
+            AddEventListener<Update<Sector>>(OnUpdate);
+            AddEventListener<Delete<Sector>>(OnDelete);
+            AddEventListener<Select<Sector>>(OnSelect);
+        }
+        protected virtual async Task<Create<Sector>> OnCreate(Create<Sector> eventData,
+            CallNext<Create<Sector>> callnext)
+        {
+            var model = eventData.Model;
+
+            if (model == null)
+                throw new ArgumentNullException("model");
+
+            eventData.ReturnValue = await DataService.CreateAsync(model);
+
+            return await callnext(eventData);
+        }
+
+        protected virtual async Task<Update<Sector>> OnUpdate(Update<Sector> eventData,
+           CallNext<Update<Sector>> callnext)
+        {
+            var model = eventData.Model;
+
+            if (model == null)
+                throw new ArgumentNullException("model");
+
+            eventData.ReturnValue = await DataService.UpdateAsync(model);
+
+            return await callnext(eventData);
+        }
+
+        protected virtual async Task<Delete<Sector>> OnDelete(Delete<Sector> eventData,
+            CallNext<Delete<Sector>> callnext)
+        {
+            var model = eventData.Model;
+
+            if (model == null)
+                throw new ArgumentNullException("model");
+
+            eventData.ReturnValue = await DataService.DeleteAsync(model);
+
+            return await callnext(eventData);
+        }
+
+        protected virtual async Task<Select<Sector>> OnSelect(Select<Sector> eventData,
+            CallNext<Select<Sector>> callnext)
+        {
+            var query = eventData.Query;
+
+            if (query == null)
+                throw new ArgumentNullException("query");
+
+            eventData.ReturnValue = await DataService.SelectAsync(query);
+
+            return await callnext(eventData);
+        }
+
+    }
+
+}
